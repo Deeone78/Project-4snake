6ee716cc469d36e47a69cc4c59883eebae65707d
Deeone78 <deeyimark@gmail.com>
2023-07-06 15:33:11 +0100

    opps

--- Snake Game/Assets/Scripts/Food.cs
+++ Snake Game/Assets/Scripts/Food.cs
@@ -6,43 +6,36 @@ public class Food : MonoBehaviour
 {
     // Start is called before the first frame update
     BoxCollider2D gridArea;
+    bool colliding = false;
 
-    private void Start()
+    private void Awake()
     {
         gridArea = GameObject.Find("GridArea").GetComponent<BoxCollider2D>();
+
+        Debug.Log(gridArea);
+
         RandomizePostion();
-    }
-
-    private void RandomizePostion()
-    {
-        Bounds bounds = this.gridArea.bounds;
-        float x = Random.Range(bounds.min.x, bounds.max.x);
-        float y = Random.Range(bounds.min.y, bounds.max.y);
-        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
-
-        /*if (Mathf.Round(x)&&Mathf.Round(y))
-        {
-            float x = Random.Range(bounds.min.x, bounds.max.x);
-            float y = Random.Range(bounds.min.y, bounds.max.y);
-            this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
-
-        }*/
-
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
         if (other.tag == "Player")
         {
-            RandomizePostion();
+           // Destroy(gameObject);
+           RandomizePostion();
         }
         
-
+        if (other.tag == "Food" || other.tag == "Obstacle"){
+            RandomizePostion();
+        }
     }
+
+    private void RandomizePostion()
+    {
+        Bounds bounds = gridArea.bounds;
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+
 }
